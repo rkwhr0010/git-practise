1804706b2a630723d1a1787846dfaf75aae38e7c
rkwhr0010 <rkwhr0010@naver.com>
2023-08-31 15:08:08 +0900

    노드 제이쿼리 모듈 추가

--- refac_makeDtlInfo.js
+++ refac_makeDtlInfo.js
@@ -1,32 +1,41 @@
 const lngeSelectType = "M";
 
 // 라운지 이용현황 상세  HTML 그리기 (그래프오른쪽 데이터)
-function makeDtlInfo({VST_CUST_CNT, VAT_TIME, LGNE_TOVRRT, DRNK_ORD_PRDC_CNT
-    , ORD_CHG_TOVRRT, SLNG_AMT, PCH_CUST_AMT}){
+function makeDtlInfo({
+    VST_CUST_CNT = '', 
+    VAT_TIME = '', 
+    LGNE_TOVRRT = '', 
+    DRNK_ORD_PRDC_CNT = '', 
+    ORD_CHG_TOVRRT = '', 
+    SLNG_AMT = '', 
+    PCH_CUST_AMT = ''
+} = {}) {
+
     //값 초기화
     DRNK_ORD_PRDC_CNT = lngeSelectType == 'M' ? ' - ' : DRNK_ORD_PRDC_CNT;
 
     //지우기
     $('#dtlInfo').children().remove();
     //그리기
-    $('#dtlInfo').append(createLoungeTag());
+    $('#dtlInfo').append(_createLoungeHtml());
 
     return;
     
-    function createLoungeTag() {
+    function _createLoungeHtml() {
         const html = [];
 
-        html.push(_createTag(`입실객수  :`, `${comma(VST_CUST_CNT)}명`));
-        html.push(_createTag(`인당 평균 체류시간  :`, `${VAT_TIME}분`));
-        html.push(_createTag(`라운지 회전율  :`, `${LGNE_TOVRRT}`));
-        html.push(_createTag(`인당 평균 음료건수  :`, `${comma(DRNK_ORD_PRDC_CNT)}잔`));
-        html.push(_createTag(`구매율  :`, `${ORD_CHG_TOVRRT}%`));
-        html.push(_createTag(`구매금액  :`, `${comma(SLNG_AMT)}백만원`));
-        html.push(_createTag(`객단가  :`, `${comma(PCH_CUST_AMT)}천원`));
+        html.push(_createListHtml(`입실객수  :`, `${comma(VST_CUST_CNT)}명`));
+        html.push(_createListHtml(`인당 평균 체류시간  :`, `${VAT_TIME}분`));
+        html.push(_createListHtml(`라운지 회전율  :`, `${LGNE_TOVRRT}`));
+        html.push(_createListHtml(`인당 평균 음료건수  :`, `${comma(DRNK_ORD_PRDC_CNT)}잔`));
+        html.push(_createListHtml(`구매율  :`, `${ORD_CHG_TOVRRT}%`));
+        html.push(_createListHtml(`구매금액  :`, `${comma(SLNG_AMT)}백만원`));
+        html.push(_createListHtml(`객단가  :`, `${comma(PCH_CUST_AMT)}천원`));
+
         return html.join('\n');
     }
 
-    function _createTag(left, right){
+    function _createListHtml(left, right){
         const tag = [];
         tag.push(`<li>`);
         tag.push(`  <span class="lanunge_left">${left}</span>`);
