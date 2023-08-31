f0de9f86c8bd06909c3720a834708ce08b1606f3
rkwhr0010 <rkwhr0010@naver.com>
2023-08-31 09:07:31 +0900

    refac_makeDtlInfo 추가 개선

--- refac_makeDtlInfo.js
+++ refac_makeDtlInfo.js
@@ -3,23 +3,28 @@ const lngeSelectType = "M";
 // 라운지 이용현황 상세  HTML 그리기 (그래프오른쪽 데이터)
 function makeDtlInfo({VST_CUST_CNT, VAT_TIME, LGNE_TOVRRT, DRNK_ORD_PRDC_CNT
     , ORD_CHG_TOVRRT, SLNG_AMT, PCH_CUST_AMT}){
-    
+    //값 초기화
     DRNK_ORD_PRDC_CNT = lngeSelectType == 'M' ? ' - ' : DRNK_ORD_PRDC_CNT;
 
-    const html = [];
-
-    html.push( _createTag(`입실객수  :`, `${comma(VST_CUST_CNT)}명`));
-    html.push( _createTag(`인당 평균 체류시간  :`, `${VAT_TIME}분`));
-    html.push( _createTag(`라운지 회전율  :`, `${LGNE_TOVRRT}`));
-    html.push( _createTag(`인당 평균 음료건수  :`, `${comma(DRNK_ORD_PRDC_CNT)}잔`));
-    html.push( _createTag(`구매율  :`, `${ORD_CHG_TOVRRT}%`));
-    html.push( _createTag(`구매금액  :`, `${comma(SLNG_AMT)}백만원`));
-    html.push( _createTag(`객단가  :`, `${comma(PCH_CUST_AMT)}천원`));
-
+    //지우기
     $('#dtlInfo').children().remove();
-    $('#dtlInfo').append(html.concat());
+    //그리기
+    $('#dtlInfo').append(createLoungeTag());
 
     return;
+    
+    function createLoungeTag() {
+        const html = [];
+
+        html.push(_createTag(`입실객수  :`, `${comma(VST_CUST_CNT)}명`));
+        html.push(_createTag(`인당 평균 체류시간  :`, `${VAT_TIME}분`));
+        html.push(_createTag(`라운지 회전율  :`, `${LGNE_TOVRRT}`));
+        html.push(_createTag(`인당 평균 음료건수  :`, `${comma(DRNK_ORD_PRDC_CNT)}잔`));
+        html.push(_createTag(`구매율  :`, `${ORD_CHG_TOVRRT}%`));
+        html.push(_createTag(`구매금액  :`, `${comma(SLNG_AMT)}백만원`));
+        html.push(_createTag(`객단가  :`, `${comma(PCH_CUST_AMT)}천원`));
+        return html.join('\n');
+    }
 
     function _createTag(left, right){
         const tag = [];
